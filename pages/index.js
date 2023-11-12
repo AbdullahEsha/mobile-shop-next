3e49184add767c2bc7c71f67ca0c52cadb0e20c0
AbdullahEsha <shahariaresha@gmail.com>
2023-11-13 02:47:18 +0600

    update

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,29 @@ import { EffectCards } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore from 'swiper'
 import Image from 'next/image'
+import { FaArrowRightLong } from 'react-icons/fa6'
+import { GiCheckMark } from 'react-icons/gi'
+import ReactImageGallery from 'react-image-gallery'
+import ReactImageMagnify from 'react-image-magnify'
+import ReactSlick from 'react-slick'
+
+// import stylesheet if you're not already using CSS @import
+import 'react-image-gallery/styles/css/image-gallery.css'
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600.jpg',
+    thumbnail: 'https://picsum.photos/id/1018/250/150.jpg',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600.jpg',
+    thumbnail: 'https://picsum.photos/id/1015/250/150.jpg',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600.jpg',
+    thumbnail: 'https://picsum.photos/id/1019/250/150.jpg',
+  },
+]
 
 // Import Swiper styles
 import 'swiper/css'
@@ -27,6 +50,13 @@ const Home = () => {
     }
     fetchCountries()
   }, [])
+
+  const [selectedImage, setSelectedImage] = useState(images[0])
+
+  const handleImageClick = (event, { index }) => {
+    // Update the selected image when a thumbnail is clicked
+    setSelectedImage(images[index])
+  }
 
   return (
     <main>
@@ -72,46 +102,80 @@ const Home = () => {
           />
         </SwiperSlide>
       </Swiper>
-      <div className="flex">
-        <div className="flex-1"></div>
+      <div className="flex ">
         <div className="flex-1">
-          <h3>Apple iPhones</h3>
-          <hr />
-          <h2>iPhone 14 Pro Max</h2>
-          <hr />
-          <div className="flex">
-            <p>Model: </p>
-            <button>iPhone 14</button>
-            <button>iPhone 14 Pro Max</button>
-          </div>
-          <div className="flex">
-            <p>Color: </p>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-          </div>
-          <div className="flex">
-            <p>Storage: </p>
-            <button>128GB</button>
-            <button>256GB</button>
-            <button>512GB</button>
-          </div>
-          <hr />
-          <div className="flex">
-            <p>Nationality: </p>
-            {/* also show flag with country name*/}
-            <select>
-              {countries.map((country) => (
-                <option key={country.name.common} value={country.name.common}>
-                  {country.name.common}
-                </option>
-              ))}
-            </select>
+          {/* Carousel Example Basic integration with react-slick. In-place enlargement for mouse and touch input.
+              Side-by-side enlargement supported, please see External Enlarged Image Demo. Responsive and fluid between breakpoints.
+              Initial file size optimized via srcset and sizes attributes. */}
+          {/* also use react-image-gallery for slider option to show multipul image */}
+          <div className="max-w-full">
+            <ReactImageGallery thumbnailPosition={'bottom'} items={images} />
           </div>
         </div>
+        <div className="flex-1 flex flex-col gap-5 mt-10">
+          <h4 className="font-semibold text-teal-800 mb-0">Apple iPhones</h4>
+          <hr />
+          <h3 className="text-[#333] mb-0">iPhone 14 Pro Max</h3>
+          <hr />
+          <div className="flex items-center gap-2">
+            <p className="w-20">Model:</p>
+            <button className="px-5 py-2 border-[1px] font-[400] border-gray-400 hover:border-teal-800 hover:text-teal-800 ease-linear delay-200 transition">
+              iPhone 14
+            </button>
+            <button className="px-5 py-2 border-[1px] font-[400] border-gray-400 hover:border-teal-800 hover:text-teal-800 ease-linear delay-200 transition">
+              iPhone 14 Pro Max
+            </button>
+          </div>
+          <div className="flex gap-2 items-center">
+            <p className="w-20">Color: </p>
+            <button className="h-12 w-12 rounded-full bg-[#301934] flex justify-center items-center">
+              <GiCheckMark size={20} color="#fff" />
+            </button>
+            <button className="h-12 w-12 rounded-full bg-[#215E7C] flex justify-center items-center">
+              <GiCheckMark size={20} color="#fff" />
+            </button>
+            <button className="h-12 w-12 rounded-full bg-[#A50011] flex justify-center items-center">
+              <GiCheckMark size={20} color="#fff" />
+            </button>
+            <button className="h-12 w-12 rounded-full bg-[#F9E5C9] flex justify-center items-center">
+              <GiCheckMark size={20} color="#fff" />
+            </button>
+            <button className="h-12 w-12 rounded-full bg-[#5C5B57] flex justify-center items-center">
+              <GiCheckMark size={20} color="#fff" />
+            </button>
+          </div>
+          <div className="flex gap-2 items-center">
+            <p className="w-20">Storage: </p>
+            <button className="px-5 py-2 border-[1px] font-[400] border-gray-400 hover:border-teal-800 hover:text-teal-800 ease-linear delay-200 transition">
+              128GB
+            </button>
+            <button className="px-5 py-2 border-[1px] font-[400] border-gray-400 hover:border-teal-800 hover:text-teal-800 ease-linear delay-200 transition">
+              256GB
+            </button>
+            <button className="px-5 py-2 border-[1px] font-[400] border-gray-400 hover:border-teal-800 hover:text-teal-800 ease-linear delay-200 transition">
+              512GB
+            </button>
+          </div>
+          <hr />
+          <div className="flex gap-2 items-center">
+            <p className="w-20">Nationality:</p>
+            <select className="px-5 py-3 border-[1px] font-[400] border-gray-400 hover:border-teal-800 hover:text-teal-800 ease-linear delay-200 transition">
+              {countries
+                .sort((a, b) => a.name.common.localeCompare(b.name.common))
+                .map((country) => (
+                  <option key={country.name.common}>
+                    {country.name.common}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <hr />
+          <button className="py-3 bg-teal-600 hover:bg-teal-500 font-bold text-sm text-white flex items-center gap-2 justify-center w-3/4 rounded-sm">
+            NEXT <FaArrowRightLong size={16} />
+          </button>
+        </div>
       </div>
+      <div className="h-20 bg-teal-400 mt-5"></div>
     </main>
   )
 }
