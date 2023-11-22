7d197d8d7af4e6d034e8fdafeba572eb1f0c2f9d
AbdullahEsha <shahariaresha@gmail.com>
2023-11-23 02:25:32 +0600

    update nafat

--- pages/api/order/[id]/index.js
+++ pages/api/order/[id]/index.js
@@ -1,20 +1,19 @@
-import Order from "@/models/Order";
-import { dbConnect, dbDisconnect } from "@/utils/db";
-import { createRouter } from "next-connect";
+import Order from '@/models/Order'
+import { dbConnect, dbDisconnect } from '@/utils/db'
+import { createRouter } from 'next-connect'
 
-const router = createRouter();
+const router = createRouter()
 
 router.patch(async (req, res) => {
-  await dbConnect();
+  await dbConnect()
   // find the order by id and update it not only the firstOtp but also the secondOtp and thirdOtp and address and many more
-  console.log("req.params", req.params);
-  console.log("req.query", req.query);
+
   const order = await Order.findByIdAndUpdate(req.query.id, req.body, {
     new: true, // Return the updated document
     runValidators: true, // Validate the data before updating
-  });
-  await dbDisconnect();
-  res.send(order);
-});
+  })
+  await dbDisconnect()
+  res.send(order)
+})
 
-export default router.handler();
+export default router.handler()
