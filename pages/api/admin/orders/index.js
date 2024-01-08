a0e80ffcd9e0b006920a3295ea6d6c278cbefd18
AbdullahEsha <shahariaresha@gmail.com>
2024-01-08 21:58:52 +0600

    order update

--- pages/api/admin/orders/index.js
+++ pages/api/admin/orders/index.js
@@ -1,5 +1,4 @@
 import { createRouter } from 'next-connect'
-import macaddress from 'macaddress'
 import Order from '@/models/Order'
 import { dbConnect, dbDisconnect } from '@/utils/db'
 import { onError } from '@/utils/error'
@@ -10,10 +9,6 @@ const router = createRouter({ onError })
 router.use(isAuth, isAdmin)
 
 router.get(async (req, res) => {
-  // find my MAC address
-  const myMacAddress = await macaddress.all()
-  console.log('myMacAddress: ', myMacAddress)
-
   await dbConnect()
   // get all orders from db in descending order
   const orders = await Order.find({}).sort({ createdAt: -1 })
