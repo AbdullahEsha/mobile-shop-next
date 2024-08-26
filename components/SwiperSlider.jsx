b37e709a57728ac66efe52bfc8b4dad00aa6fd49
Md. Nuruzaman Milon <njmilon1@gmail.com>
2024-08-26 21:17:04 +0600

    updating home carosel image

--- components/SwiperSlider.jsx
+++ components/SwiperSlider.jsx
@@ -1,19 +1,17 @@
-import { Pagination, Autoplay, A11y, EffectCards } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore from 'swiper'
+import { Pagination, Autoplay, A11y, EffectCards } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
 
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
-import 'swiper/css/free-mode'
-import 'swiper/css/thumbs'
-import { FaArrowRightLong } from 'react-icons/fa6'
-import { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/free-mode";
+import "swiper/css/thumbs";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const SwiperSlider = () => {
-  SwiperCore.use([Autoplay])
+  SwiperCore.use([Autoplay]);
 
   return (
     <Swiper
@@ -30,34 +28,40 @@ const SwiperSlider = () => {
       // onSwiper={(swiper) => console.log(swiper)}
       // onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide className="!h-[300px] w-full bg-[url('/images/slider-background-girl.jpg')] bg-center bg-cover bg-no-repeat">
-        <div className="h-[300px] flex flex-col justify-center items-end mx-40 ">
-          <h1 className="text-4xl font-bold text-gray-600">IPhone 14</h1>
-          <button className="py-3 px-5 w-fit text-gray-600 hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
+      <SwiperSlide className="!h-[300px] xl:!h-[350px] 2xl:!h-[450px] w-full bg-[url('/images/iphone15.png')] bg-center bg-cover bg-no-repeat">
+        <div className="h-[300px] flex flex-col justify-center items-center lg:items-end lg:mx-40">
+          <h1 className="text-4xl font-bold text-white">Dreaming iPhone</h1>
+          <button className="py-3 px-5 w-fit text-white hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
             Order Now <FaArrowRightLong size={16} />
           </button>
         </div>
       </SwiperSlide>
-      <SwiperSlide className="!h-[300px] w-full bg-[url('/images/slider-background-human.jpg')] bg-center bg-cover bg-no-repeat">
-        <div className="h-[300px] flex flex-col justify-center items-end mx-40 ">
-          <h1 className="text-4xl font-bold text-gray-600">
-            IPhone 14 Pro Max
-          </h1>
-          <button className="py-3 px-5 w-fit text-gray-600 hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
+      <SwiperSlide className="!h-[300px] xl:!h-[350px] 2xl:!h-[450px] w-full bg-[url('/images/iphone14.png')] bg-center bg-cover bg-no-repeat">
+        <div className="h-[300px] flex flex-col justify-center items-center lg:items-end lg:mx-40">
+          <h1 className="text-4xl font-bold text-white">IPhone 15 Pro Max</h1>
+          <button className="py-3 px-5 w-fit text-white hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
             Order Now <FaArrowRightLong size={16} />
           </button>
         </div>
       </SwiperSlide>
-      <SwiperSlide className="!h-[300px] w-full bg-[url('/images/slider-background.jpg')] bg-center bg-cover bg-no-repeat">
-        <div className="h-[300px] flex flex-col justify-center items-start mx-40">
-          <h1 className="text-4xl font-bold text-gray-600">Buy Your Dreams</h1>
-          <button className="py-3 px-5 w-fit text-gray-600 hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
+      <SwiperSlide className="!h-[300px] xl:!h-[350px] 2xl:!h-[450px] w-full bg-[url('/images/iphone4.jpeg')] bg-top bg-cover bg-no-repeat">
+        <div className="h-[300px] flex flex-col justify-center items-center lg:items-end lg:mx-40">
+          <h1 className="text-4xl font-bold text-white">IPhone 14 Pro Max</h1>
+          <button className="py-3 px-5 w-fit text-gray-700 bg-gray-200 hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
+            Order Now <FaArrowRightLong size={16} />
+          </button>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className="!h-[300px] xl:!h-[350px] 2xl:!h-[450px] w-full bg-[url('/images/iphone3.png')] bg-top bg-cover bg-no-repeat">
+        <div className="h-[300px] flex flex-col justify-center items-center lg:items-end lg:mx-40">
+          <h1 className="text-4xl font-bold text-white">Buy Your Dreams</h1>
+          <button className="py-3 px-5 w-fit text-white hover:text-white hover:bg-teal-500 font-bold border-gray-500 hover:border-teal-500 border text-sm flex items-center gap-2 justify-center rounded-sm transition-all delay-150 ease-in-out">
             Order Now <FaArrowRightLong size={16} />
           </button>
         </div>
       </SwiperSlide>
     </Swiper>
-  )
-}
+  );
+};
 
-export default SwiperSlider
+export default SwiperSlider;
